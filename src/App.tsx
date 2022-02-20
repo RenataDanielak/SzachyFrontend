61e27aff8021aad9cba2782ad1b87c31ac34d78f
Renata <ren.danielak@gmail.com>
2022-02-20 20:02:13 +0100

    poczatek

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,186 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import logo from './logo.svg';
+import { Field, Form } from 'react-final-form'
+import { OnChange } from 'react-final-form-listeners'
 import './App.css';
+import store,{ AppState, dostepneRuchySelector, IRuchRequest, mozliweRuchy, planszaSelector, pobierzPlansze, ruch, sprawdzanyPionekSelector, useTypedSelector } from './redux.config'
+import { FormApi } from 'final-form';
+import { useSelector, useStore } from 'react-redux';
 
 function App() {
+
+const submit = (values: IRuchRequest) => {
+  store.dispatch(ruch({
+    startX: values.startX,
+    startY: values.startY,
+    endX: values.endX,
+    endY: values.endY
+  }))
+} 
+
+const pokazMozliweRuchy = (x: number | undefined, y: number | undefined) : void => {
+  if (x && y) {
+    store.dispatch(mozliweRuchy({startX: x, startY: y}))
+  }
+
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Szachy
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+      {/* <Welcome name={store.getState() && store.getState().dostepneRuchy != undefined 
+          ? store.getState().dostepneRuchy!.map(ruch =>ruch.pozycjaX) 
+          : 'Brak dostepnych ruchow'} />; */}
+
+      <DostepneRuchyComp/>
+
+      <Form onSubmit={submit} render={({ handleSubmit, form }) => (
+        <form onSubmit={handleSubmit}>
+          <Field 
+            name='startX'
+            component="input"
+            type="text">
+          </Field>
+          <OnChange name='startX'>
+            {(value) : void => {
+              const y = form.getFieldState('startY')?.value;
+              pokazMozliweRuchy(value, y);
+
+            }}
+          </OnChange>
+          <Field 
+            name='startY'
+            component="input"
+            type="text">
+          </Field>
+          <OnChange name='startY'>
+            {(value) : void => {
+              const x = form.getFieldState('startX')?.value;
+              pokazMozliweRuchy(x, value);
+
+            }}
+          </OnChange>
+          <br/>
+          <Field 
+            name='endX'
+            component="input"
+            type="text">
+          </Field>
+          <Field 
+            name='endY'
+            component="input"
+            type="text">
+          </Field>
+          <br/>
+          <button type="submit">
+              Wykonaj ruch
+            </button>
+
+        </form>
+      )}
+      />
+      
       </header>
+
     </div>
   );
 }
 
 export default App;
+
+// function Welcome(props: any) {  
+//   //const test = useSelector<AppState>((state) => state.dostepneRuchy.pozycjaX);
+//   //const store = useStore();
+//   {store.getState().dostepneRuchy}
+//   return <h1>Cześć, {props.name}</h1> ;
+//   //return <h1>Cześć, {props.name}, {store.getState()}</h1> ;
+// }
+
+const DostepneRuchyComp: React.FC = () => {
+  //const test2 = useSelector<AppState>(dostepneRuchySelector);
+
+  const dostepneRuchy = useTypedSelector(dostepneRuchySelector); 
+  const sprawdzanyPionek = useTypedSelector(sprawdzanyPionekSelector); 
+  const plansza = useTypedSelector(planszaSelector); 
+
+  useEffect(() => {
+    store.dispatch(pobierzPlansze())
+  }, []);
+
+  useEffect(() => {
+    let c = document.getElementById("myCanvas") as HTMLCanvasElement;;
+    if (!!c) {
+      let ctx = c.getContext("2d");
+      if (!!ctx) {
+        ctx.fillStyle = "white"
+        ctx.fillRect(0,0,400,400)
+        for (let i = 0; i < 8; i++) {
+          const przesuniecie = i%2;
+          for (let j = 0; j < 4; j++) {
+            ctx.fillStyle = "black"
+            ctx.fillRect(j*100 + 50 * przesuniecie, i*50 ,50,50)
+          }
+        }
+        console.log("PLANSZA:" + plansza);
+        plansza?.map(pionek => {
+          if(!!ctx) {
+            ctx.beginPath();
+          const x=pionek.pozycjaX;
+          const y=pionek.pozyjcjaY;
+          if (x !== undefined && y !== undefined) {
+            ctx.arc(x*50+25, 400-y*50-25, 20, 0, 2 * Math.PI); 
+            ctx.fillStyle = pionek.kolor === 'BIALY' ? "white" : "black"
+            ctx.fill();
+            ctx.strokeStyle = 'blue';
+            ctx.lineWidth = 3;
+            ctx.stroke(); 
+          }
+          
+
+          }
+        })
+
+        if(sprawdzanyPionek) {
+          ctx.beginPath();
+          const x1=sprawdzanyPionek.startX;
+          const y1=sprawdzanyPionek.startY;
+          ctx.strokeStyle = 'red';
+          ctx.rect(x1*50, 350-y1*50, 50, 50);
+          ctx.stroke();
+        }
+        
+        if(dostepneRuchy) {
+          dostepneRuchy.map(ruch => {
+            if (ctx) {
+              ctx.beginPath();
+              const x2=ruch.pozycjaX;
+              const y2=ruch.pozycjaY;
+              if(x2 !== undefined && y2!==undefined) {
+                ctx.strokeStyle = 'red';
+                ctx.moveTo(x2*50, 400-y2*50);
+                ctx.lineTo(x2*50+50, 400-y2*50-50);
+                ctx.moveTo(x2*50+50, 400-y2*50);
+                ctx.lineTo(x2*50+0, 400-y2*50-50);
+                ctx.stroke();
+              }              
+            }
+          })
+        }
+        
+      }
+    }
+  }, [dostepneRuchy,sprawdzanyPionek, plansza]);
+  
+  return ( 
+  <Fragment>
+    <canvas id="myCanvas" width="400" height="400"></canvas>
+  </Fragment>
+  )
+  //return <h1>Dostepne Ruchy </h1>
+} 
---
+++ src/redux.config.ts
@@ -0,0 +1,164 @@
+import { configureStore, createAction, createReducer, Selector } from '@reduxjs/toolkit';
+import axios from 'axios';
+import React from 'react';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { combineEpics, createEpicMiddleware, Epic, ofType } from 'redux-observable';
+import { from, of, pluck, switchMap } from 'rxjs';
+import { ParametryPolaDto, PionekDto, SzachyControllerApi } from './model/api';
+
+export interface IChessState {
+    sprawdzanyPionek?: {
+        startX: number;
+        startY: number;
+    }
+    wykonywanyRuch: {    //PO DODANIU EPICKU DO USUNIECIA
+        startX: number;
+        startY: number;
+        endX: number;
+        endY: number;
+    }
+    dostepneRuchy?: ParametryPolaDto[];
+    plansza?: PionekDto[];
+}
+
+export interface IMozliweRuchyRequest {
+  startX: number;
+  startY: number;
+}
+
+export interface IRuchRequest {
+    startX: number;
+    startY: number;
+    endX: number;
+    endY: number;
+}
+
+export const pobierzPlansze = createAction<undefined>('chess:pobierzPlansze')
+
+export const pobierzPlanszeSuccess = createAction<PionekDto[]>('chess:pobierzPlanszeSuccess')
+
+export const mozliweRuchy = createAction<IMozliweRuchyRequest>('chess:mozliweRuchy')
+
+export const mozliweRuchySuccess = createAction<ParametryPolaDto[]>('chess:mozliweRuchySuccess')
+
+export const ruch = createAction<IRuchRequest>('chess:wykonajRuch')
+
+function chessReducerOld(state = { valueX: 0, valueY: 0 }, action: { type: any }) {
+    switch (action.type) {
+      case 'wykonajRuch':
+        return { ...state, valueX: state.valueX + 1 }
+      default:
+        return state
+    }
+  }
+
+const initCheseState: IChessState = {
+    wykonywanyRuch: { startX: 0, startY: 0, endX:0, endY:0 }
+}
+
+const chessReducer = createReducer(initCheseState, (builder) => {
+    builder
+      .addCase(ruch, (state, action) => { 
+        state.wykonywanyRuch.startX = action.payload.startX
+        state.wykonywanyRuch.startY = action.payload.startY
+        state.wykonywanyRuch.endX = action.payload.endX
+        state.wykonywanyRuch.endY = action.payload.endY
+      })
+      .addCase(mozliweRuchy, (state, action) => {
+        state.sprawdzanyPionek = {startX: action.payload.startX, startY: action.payload.startY}
+      })
+      .addCase(mozliweRuchySuccess, (state, action) => {
+      state.dostepneRuchy = action.payload
+      })
+      .addCase(pobierzPlanszeSuccess, (state, action) => {
+          state.plansza = action.payload
+          state.sprawdzanyPionek = undefined
+          state.dostepneRuchy = []
+          })
+
+  })
+
+
+ 
+  export const dostepneRuchySelector: Selector<AppState, 
+  ParametryPolaDto[] | undefined> = (state) => state.dostepneRuchy;
+
+  export const sprawdzanyPionekSelector: Selector<AppState, 
+  { startX: number; startY: number; } | undefined> = (state) => state.sprawdzanyPionek;
+
+  export const planszaSelector: Selector<AppState, 
+  PionekDto[] | undefined> = (state) => state.plansza;
+
+
+
+  export interface IEpicDepenencies {
+    chessApi: SzachyControllerApi
+  }
+
+  export const ruchEpic: Epic<any, any, any, IEpicDepenencies> = (action, _, { chessApi }) =>
+    action.pipe(
+      ofType(ruch.type),
+      pluck('payload'),
+      switchMap((payload: IRuchRequest)=> 
+        from(chessApi.ruchUsingGET(payload.endX, payload.endY, payload.startX, payload.startY)).pipe(
+          switchMap((response) => of(
+            response.data 
+            ? pobierzPlansze()
+            : alert("ruch niedozwolony")
+            ))
+        )
+      )
+    );
+
+    export const pobierzPlaszeEpic: Epic<any, any, any, IEpicDepenencies> = (action, _, { chessApi }) =>
+    action.pipe( ofType(pobierzPlansze.type),
+    pluck('payload'),
+    switchMap((payload: IMozliweRuchyRequest)=> 
+      from(chessApi.planszaUsingGET()).pipe(
+        switchMap((response) => of(
+          pobierzPlanszeSuccess(response.data) 
+          ))
+      )
+    )
+  );
+
+    export const mozliwyRuchEpic: Epic<any, any, any, IEpicDepenencies> = (action, _, { chessApi }) =>
+    action.pipe(
+      ofType(mozliweRuchy.type),
+      pluck('payload'),
+      switchMap((payload: IMozliweRuchyRequest)=> 
+        from(chessApi.dostepneRuchyUsingGET(payload.startX, payload.startY)).pipe(
+          switchMap((response) => of(
+            mozliweRuchySuccess(response.data) 
+            ))
+        )
+      )
+    );
+
+  export const chessEpics = combineEpics(ruchEpic, mozliwyRuchEpic, pobierzPlaszeEpic);
+
+  const axiosInstance = axios.create();
+
+  const epicMiddleware = createEpicMiddleware({
+    dependencies: {
+      chessApi: new SzachyControllerApi(undefined, 'http:///localhost:8080', axiosInstance)
+    }
+  })
+
+
+
+
+const store = configureStore({
+  reducer: chessReducer,
+  middleware: [epicMiddleware]
+});
+
+epicMiddleware.run(chessEpics);
+
+export const useTypedSelector: TypedUseSelectorHook<AppState> = useSelector;
+
+export type AppState = ReturnType<typeof store.getState >
+export default store;
+
+
+
